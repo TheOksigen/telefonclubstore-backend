e109bed1a02ba341f2680f0df34fb8f2710f6951
@david <david@>
2024-07-25 16:05:40 +0400

    clean code

--- src/controllers/LoginControllers/login.js
+++ src/controllers/LoginControllers/login.js
@@ -6,14 +6,14 @@ const prisma = new PrismaClient();
 
 const loginSchema = z.object({
     login: z.string()
-       .min(3, { message: 'Login must be at least 3 characters long' })
-       .max(20, { message: 'Login must be less than 255 characters' })
-       .trim()
-       .min(1, { message: 'Login is required' }),
+        .min(3, { message: 'Login must be at least 3 characters long' })
+        .max(20, { message: 'Login must be less than 255 characters' })
+        .trim()
+        .min(1, { message: 'Login is required' }),
     password: z.string()
-       .min(8, { message: 'Password must be at least 8 characters long' })
-       .trim()
-       .min(1, { message: 'Password is required' })
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .trim()
+        .min(1, { message: 'Password is required' })
 })
 
 const login = async (req, res) => {
@@ -39,7 +39,7 @@ const login = async (req, res) => {
 
         const token = jwt.sign({ userid: existingUser.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.status(200).json({ token });
+        res.status(200).json({ token, status: true });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
