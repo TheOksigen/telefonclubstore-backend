496538401a7f18d76a28d4e250a3d13606686531
@david <david@>
2024-07-28 00:53:31 +0400

    modified:   src/middlewares/upload.middleware.js

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,27 +1,31 @@
+const { S3Client } = require('@aws-sdk/client-s3')
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
+const path = require('path');
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
 });
 
-const s3 = new AWS.S3();
-
 const upload = multer({
+
     storage: multerS3({
         s3: s3,
         bucket: 'telefonclubb',
-        acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
         key: function (req, file, cb) {
-            cb(null, `${Date.now().toString()}_${file.originalname}`);
-        }
-    })
-});
+            cb(null, `${Date.now().toString()}.${path.extname(file.originalname)}`);
+        },
 
-module.exports = upload;
+
+    })
+})
+
+
+module.exports = upload
