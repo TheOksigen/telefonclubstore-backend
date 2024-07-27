4fa174e47c5de63452d0e659548116aa89ba7343
@david <david@>
2024-07-28 00:51:56 +0400

    modified:   index.htm 	modified:   src/middlewares/upload.middleware.js 	modified:   src/routes/products.js

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,31 +1,27 @@
-const { S3Client } = require('@aws-sdk/client-s3')
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const path = require('path');
+const AWS = require('aws-sdk');
 
-const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 
-const upload = multer({
+const s3 = new AWS.S3();
 
+const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: 'telefonclubb',
+        acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
         key: function (req, file, cb) {
-            cb(null, `${Date.now().toString()}.${path.extname(file.originalname)}`);
-        },
-
-
+            cb(null, `${Date.now().toString()}_${file.originalname}`);
+        }
     })
-})
+});
 
-
-module.exports = upload
+module.exports = upload;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,8 @@
+// src/routes/products.js
 const express = require('express');
 const router = express.Router();
 
 const { createProduct, getProducts, getProductById, editProduct, deleteProductById, searchProduct } = require('../controllers/products.controllers');
-
 const auth = require('../middlewares/auth.middleware');
 const upload = require('../middlewares/upload.middleware');
 const validator = require('../middlewares/validation.middleware');
@@ -12,7 +12,7 @@ const { productSchema } = require('../schema/product.schema');
 router.post('/', auth, upload.array("img", 5), createProduct);
 router.get('/', getProducts);
 router.get('/id/:id', getProductById);
-router.put('/:id', auth,  upload.array("img", 5), editProduct);
+router.put('/:id', auth, upload.array("img", 5), editProduct);
 router.get('/search', searchProduct);
 router.delete('/:id', auth, deleteProductById);
 
