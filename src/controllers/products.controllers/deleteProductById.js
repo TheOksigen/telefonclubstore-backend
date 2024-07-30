543ba08610743303ed46bfebd651d6172707be80
@david <david@>
2024-07-30 11:40:05 +0400

    modified:   src/controllers/products.controllers/deleteProductById.js

--- src/controllers/products.controllers/deleteProductById.js
+++ src/controllers/products.controllers/deleteProductById.js
@@ -4,25 +4,26 @@ const prisma = new PrismaClient();
 
 const deleteCategoryByIdSchema = z.object({
     id: z.number()
-       .int({ message: 'Product ID must be an integer' })
-       .min(1, { message: 'Product ID is required' })
-})
+        .int({ message: 'Product ID must be an integer' })
+        .min(1, { message: 'Product ID is required' })
+});
 
 const deleteProductById = async (req, res) => {
-    const parseResult = deleteCategoryByIdSchema.safeParse(req.params);
+    const params = { id: Number(req.params.id) };
+
+    const parseResult = deleteCategoryByIdSchema.safeParse(params);
     if (!parseResult.success) {
         return res.status(400).json({ errors: parseResult.error.format() });
     }
     try {
-        const { id } = req.params;
+        const { id } = params;
         await prisma.product.delete({
-            where: { id: Number(id) }
+            where: { id }
         });
         res.status(200).json({ message: 'Product deleted successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
-
 
 module.exports = deleteProductById;
