9f14840666768ea1b0fee649c2162514b85b3553
@david <david@>
2024-07-30 14:37:26 +0400

    optimization

--- src/controllers/categories.controllers/createCategory.js
+++ src/controllers/categories.controllers/createCategory.js
@@ -1,9 +1,22 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const { z } = require("zod");
 
-const createCategory = async (req, res) => {    
+const categorySchema = z.object({
+    categoryName: z.string()
+        .min(3, { message: 'Category name must be at least 3 characters long' })
+        .max(50, { message: 'Category name must be less than 255 characters' })
+        .trim()
+        .min(1, { message: 'Category name is required' })
+});
+
+const createCategory = async (req, res) => {
+    const parseResult = categorySchema.safeParse(req.body);
+    if (!parseResult.success) {
+        return res.status(400).json({ errors: parseResult.error.format() });
+    }
     try {
-        const { categoryName } = req.body;
+        const { categoryName } = parseResult.data;
         const category = await prisma.category.create({
             data: { categoryName }
         });
--- src/controllers/categories.controllers/createSubcategory.js
+++ src/controllers/categories.controllers/createSubcategory.js
@@ -1,13 +1,33 @@
 const { PrismaClient } = require('@prisma/client');
+const { z } = require('zod');
 const prisma = new PrismaClient();
 
+const subcategorySchema = z.object({
+    categoryName: z.string()
+        .min(3, { message: 'Subcategory name must be at least 3 characters long' })
+        .max(25, { message: 'Subcategory name must be less than 255 characters' })
+        .trim()
+        .min(1, { message: 'Subcategory name is required' }),
+    categoryId: z.number()
+        .int({ message: 'Category ID must be an integer' })
+        .min(1, { message: 'Category ID is required' })
+});
+
 const createSubcategory = async (req, res) => {
-       try {
-        const { categoryName, categoryId } = req.body;
+    const parseResult = subcategorySchema.safeParse({
+        ...req.body,
+        categoryId: Number(req.body.categoryId),
+    });
+    if (!parseResult.success) {
+        return res.status(400).json({ errors: parseResult.error.format() });
+    }
+
+    try {
+        const { categoryName, categoryId } = parseResult.data;
         const subcategory = await prisma.subcategory.create({
             data: {
                 categoryName,
-                categoryId: Number(categoryId)
+                categoryId
             }
         });
         res.status(201).json(subcategory);
