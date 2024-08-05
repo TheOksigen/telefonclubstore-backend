6acee12b929d7dfe1759b4c1168d1b38112468f0
@david <david@yetim.me>
2024-08-05 17:00:41 +0400

    modified:   src/controllers/categories.controllers/deleteCategoryById.js

--- src/controllers/categories.controllers/deleteCategoryById.js
+++ src/controllers/categories.controllers/deleteCategoryById.js
@@ -9,7 +9,7 @@ const deleteCategoryByIdSchema = z.object({
 });
 
 const deleteCategoryById = async (req, res) => {
-    const parseResult = deleteCategoryByIdSchema.safeParse({ id: Number(req.params.id) });
+    const parseResult = deleteCategoryByIdSchema.parse({ id: Number(req.params.id) });
     if (!parseResult.success) {
         return res.status(400).json({ errors: parseResult.error.format() });
     }
