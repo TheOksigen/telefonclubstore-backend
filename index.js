00219028a693b5d2b4f3183fdceb1f328f60ee46
@david <david@yetim.me>
2024-08-06 00:18:20 +0400

    modified:   index.htm 	modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 require('dotenv').config();
 
 const productsRouter = require('./src/routes/products');
