601c00812cd3d81f106e35067c21451168c12124
@david <david@yetim.me>
2024-08-06 00:04:14 +0400

    modified:   index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+require('dotenv').config();
 
 const productsRouter = require('./src/routes/products');
 const categoriesRouter = require('./src/routes/categories');
 const loginRouter = require('./src/routes/login');
 
-require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/products', cors(), productsRouter);
-app.use('/categories', cors(), categoriesRouter);
-app.use('/auth', cors(), loginRouter);
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
+app.use('/auth', loginRouter);
 
 const PORT = process.env.PORT || 3000;
 
