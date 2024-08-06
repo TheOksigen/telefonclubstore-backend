92f88a2f11b724c54004d149b7b723ecc61a8d59
@david <david@yetim.me>
2024-08-06 20:29:29 +0400

    modified:   index.htm 	modified:   src/controllers/products.controllers/getProducts.js 	modified:   src/routes/categories.js 	modified:   src/routes/products.js

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -4,21 +4,26 @@ const {
     createSubcategory,
     editCategoriesById,
     getCategories,
-    getCategoriesById, 
+    getCategoriesById,
     deleteCategoryById,
     updateSubcategory,
     deleteSubcategory
 } = require('../controllers/categories.controllers');
 const auth = require('../middlewares/auth.middleware');
+
+const validator = require('../middlewares/validation.middleware');
+const { categorySchema, subcategorySchema } = require('../schema/categories.schema');
 const router = express.Router();
 
-router.post('/create', auth, createCategory);
+router.post('/create', validator(categorySchema), auth, createCategory);
 router.get('/all', getCategories);
 router.get('/get/:id', getCategoriesById);
-router.put('/update/:id', auth, editCategoriesById);
+router.put('/update/:id', validator(categorySchema), auth, editCategoriesById);
 router.delete('/delete/:id', auth, deleteCategoryById);
-router.post('/subcategory/create', auth, createSubcategory);
-router.put('/subcategory/update/:id', auth, updateSubcategory);
+
+
+router.post('/subcategory/create', validator(subcategorySchema), auth, createSubcategory);
+router.put('/subcategory/update/:id', validator(subcategorySchema) , auth, updateSubcategory);
 router.delete('/subcategory/delete/:id', auth, deleteSubcategory);
 
 module.exports = router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -17,7 +17,7 @@ const auth = require('../middlewares/auth.middleware');
 const upload = require('../middlewares/upload.middleware');
 
 router.post('/create', auth, upload.array('img', 5), createProduct);
-router.get('/all', getProducts);
+router.get('/all/:page/:limit', getProducts);// page seyfeni deyir  limit hemen seyfede olan itemlerin sayini gosterir 
 router.get('/get/:id', getProductById);
 router.put('/update/:id', auth, upload.array('img', 5), editProduct);
 router.get('/search', searchProduct);
