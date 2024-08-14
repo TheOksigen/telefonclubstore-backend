24d6a8d6f6f8acb97829c869322f959e7b8f643c
@david <david@yetim.me>
2024-08-14 16:56:17 +0400

    modified:   src/controllers/products.controllers/getProductById.js

--- src/controllers/products.controllers/getProductById.js
+++ src/controllers/products.controllers/getProductById.js
@@ -11,7 +11,7 @@ const getProductByIdSchema = z.object({
 const getProductById = async (req, res) => {
   const parseResult = getProductByIdSchema.parse({ id: Number(req.params.id) });
   if (!parseResult.success) {
-    return res.status(400).json({ errors: parseResult.error.format() });
+    return res.status(400).json({ errors: parseResult});
   }
   try {
     const { id } = parseResult.data;
