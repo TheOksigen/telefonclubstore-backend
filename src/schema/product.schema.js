9e14a0813ac06cc928a8cbc316cb643fd3ca0066
@david <david@yetim.me>
2024-08-20 01:52:38 +0400

    modified:   src/controllers/products.controllers/createProduct.js 	modified:   src/schema/product.schema.js

--- src/schema/product.schema.js
+++ src/schema/product.schema.js
@@ -5,20 +5,19 @@ const productSchema = z.object({
     name: z.string()
         .min(3, { message: 'Product name must be at least 3 characters long' })
         .max(255, { message: 'Product name must be less than 255 characters' })
-        .trim()
-        .nonempty({ message: 'Product name is required' }),
-    price: z.number()
+        .trim(),        
+    price: z.number().nonpositive()
         .positive({ message: 'Price must be a positive number' })
         .min(0.01, { message: 'Price must be at least 0.01' })
         .max(10000, { message: 'Price must be less than 10,000' }),
-    discount: z.number()
+    discount: z.number().nonpositive()
         .min(0, { message: 'Discount must be at least 0' })
         .int({ message: 'Discount must be an integer' })
         .max(100, { message: 'Discount must be less than or equal to 100' }),
-    categoryId: z.number()
+    categoryId: z.number().nonpositive()
         .int({ message: 'Category ID must be an integer' })
         .positive({ message: 'Category ID must be a positive integer' }),
-    description: z.string()
+    description: z.string().nonpositive()
         .min(3, { message: 'Product description must be at least 3 characters long' })
         .max(255, { message: 'Product description must be less than 255 characters' })
         .trim(),
